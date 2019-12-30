bbe3e87048e3c962a5bb17f0cfb54d3911aecd28
Mlinfeng <50072711+Mlinfeng@users.noreply.github.com>
2019-12-30 18:49:46 +0800

    修复真机调试不能挑战新闻详情页的bug

--- pages/news/news.js
+++ pages/news/news.js
@@ -8,20 +8,20 @@ const shuffle = require('../../utils/shuffle.js')
 Page({
   data: {
     headerTitleName: [
-      { name: '头条', nameID: '201701', newsType: 'top' },
-      { name: '军事', nameID: '201702', newsType: 'junshi' },
-      { name: '体育', nameID: '201703', newsType: 'tiyu' },
-      { name: '科技', nameID: '201704', newsType: 'keji' },
-      { name: '财经', nameID: '201705', newsType: 'caijing' },
-      { name: '社会', nameID: '201706', newsType: 'shehui' },
-      { name: '时尚', nameID: '201707', newsType: 'shishang' },
-      { name: '娱乐', nameID: '201708', newsType: 'yule' },
-      { name: '国内', nameID: '201709', newsType: 'guonei' },
-      { name: '国际', nameID: '2017010', newsType: 'guoji' }
+      { name: '头条', nameID: '201901', newsType: 'top' },
+      { name: '军事', nameID: '201902', newsType: 'junshi' },
+      { name: '体育', nameID: '201903', newsType: 'tiyu' },
+      { name: '科技', nameID: '201904', newsType: 'keji' },
+      { name: '财经', nameID: '201905', newsType: 'caijing' },
+      { name: '社会', nameID: '201906', newsType: 'shehui' },
+      { name: '时尚', nameID: '201907', newsType: 'shishang' },
+      { name: '娱乐', nameID: '201908', newsType: 'yule' },
+      { name: '国内', nameID: '201909', newsType: 'guonei' },
+      { name: '国际', nameID: '2019010', newsType: 'guoji' }
     ],
     swiperIndex: '1/4',
     topPic: [],
-    tapID: 201701, // 判断是否选中
+    tapID: 201901, // 判断是否选中
     contentNewsList: [],
     showCopyright: false,
     refreshing: false
